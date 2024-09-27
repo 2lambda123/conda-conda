bf3362bd66a72a5a309c8cc571286bc2792cc36d
Ken Odegard <kodegard@anaconda.com>
2024-09-27 16:39:49 -0500

    Skip xonsh on Windows since its harder to install correctly

--- tests/activate/test_xonsh.py
+++ tests/activate/test_xonsh.py
@@ -2,6 +2,12 @@
 # SPDX-License-Identifier: BSD-3-Clause
 from shutil import which
 
+import pytest
+
+from conda.common.compat import on_win
+
+pytestmark = pytest.mark.skipif(on_win, reason="xonsh is harder to install on Windows")
+
 
 def test_xonsh_available():
     assert which("xonsh")
