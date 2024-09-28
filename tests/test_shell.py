b8412c8d93cf35468cf37201d0314e7163141b83
Ken Odegard <kodegard@anaconda.com>
2024-09-27 20:04:20 -0500

    Move InteractiveShell to tests/shell

--- tests/test_shell.py
+++ tests/test_shell.py
@@ -7,30 +7,33 @@ import platform
 import sys
 from functools import lru_cache
 from logging import getLogger
-from os.path import dirname, join
+from os.path import join
 from pathlib import Path
 from re import escape
 from shutil import which
-from signal import SIGINT
 from subprocess import CalledProcessError, check_output
 from typing import TYPE_CHECKING
 from uuid import uuid4
 
 import pytest
-from pexpect.popen_spawn import PopenSpawn
 
-from conda import CONDA_PACKAGE_ROOT, CONDA_SOURCE_ROOT
+from conda import CONDA_PACKAGE_ROOT
 from conda import __version__ as conda_version
-from conda.activate import activator_map, native_path_to_unix
 from conda.base.context import context
 from conda.common.compat import on_win
 from conda.testing.integration import SPACER_CHARACTER
-from conda.utils import quote_for_shell
 
-from .shell import ACTIVATE_ARGS, DEACTIVATE_ARGS, DEV_ARG, HDF5_VERSION, INSTALL_ARGS
+from .shell import (
+    ACTIVATE_ARGS,
+    DEACTIVATE_ARGS,
+    DEV_ARG,
+    HDF5_VERSION,
+    INSTALL_ARGS,
+    InteractiveShell,
+)
 
 if TYPE_CHECKING:
-    from typing import Any, Callable, Iterable, Iterator
+    from typing import Any, Callable, Iterator
 
     from pytest import TempPathFactory
 
@@ -70,228 +73,6 @@ skip_unsupported_bash = pytest.mark.skipif(
     bool(bash_unsupported()),
     reason=bash_unsupported() or "bash: supported!",
 )
-
-
-class InteractiveShellType(type):
-    EXE = quote_for_shell(native_path_to_unix(sys.executable))
-    SHELLS: dict[str, dict] = {
-        "posix": {
-            "activator": "posix",
-            "init_command": (
-                f'eval "$({EXE} -m conda shell.posix hook {DEV_ARG})" '
-                # want CONDA_SHLVL=0 before running tests so deactivate any active environments
-                # since we do not know how many environments have been activated by the user/CI
-                # just to be safe deactivate a few times
-                "&& conda deactivate "
-                "&& conda deactivate "
-                "&& conda deactivate "
-                "&& conda deactivate"
-            ),
-            "print_env_var": 'echo "$%s"',
-        },
-        "bash": {
-            # MSYS2's login scripts handle mounting the filesystem. Without it, /c is /cygdrive.
-            "args": ("-l",) if on_win else (),
-            "base_shell": "posix",  # inheritance implemented in __init__
-        },
-        "dash": {"base_shell": "posix"},
-        "zsh": {"base_shell": "posix"},
-        # It should be noted here that we use the latest hook with whatever conda.exe is installed
-        # in sys.prefix (and we will activate all of those PATH entries).  We will set PYTHONPATH
-        # though, so there is that.  What I'm getting at is that this is a huge mixup and a mess.
-        "cmd.exe": {
-            "activator": "cmd.exe",
-            # For non-dev-mode you'd have:
-            #            'init_command': 'set "CONDA_SHLVL=" '
-            #                            '&& @CALL {}\\shell\\condabin\\conda_hook.bat {} '
-            #                            '&& set CONDA_EXE={}'
-            #                            '&& set _CE_M='
-            #                            '&& set _CE_CONDA='
-            #                            .format(CONDA_PACKAGE_ROOT, dev_arg,
-            #                                    join(sys.prefix, "Scripts", "conda.exe")),
-            "init_command": (
-                '@SET "CONDA_SHLVL=" '
-                f"&& @CALL {CONDA_PACKAGE_ROOT}\\shell\\condabin\\conda_hook.bat {DEV_ARG} "
-                f'&& @SET "CONDA_EXE={sys.executable}" '
-                '&& @SET "_CE_M=-m" '
-                '&& @SET "_CE_CONDA=conda"'
-            ),
-            "print_env_var": "@ECHO %%%s%%",
-        },
-        "csh": {
-            "activator": "csh",
-            # Trying to use -x with `tcsh` on `macOS` results in some problems:
-            # This error from `PyCharm`:
-            # BrokenPipeError: [Errno 32] Broken pipe (writing to self.proc.stdin).
-            # .. and this one from the `macOS` terminal:
-            # pexpect.exceptions.EOF: End Of File (EOF).
-            # 'args': ('-x',),
-            "init_command": (
-                f'set _CONDA_EXE="{CONDA_PACKAGE_ROOT}/shell/bin/conda"; '
-                f"source {CONDA_PACKAGE_ROOT}/shell/etc/profile.d/conda.csh;"
-            ),
-            "print_env_var": 'echo "$%s"',
-        },
-        "tcsh": {"base_shell": "csh"},
-        "fish": {
-            "activator": "fish",
-            "init_command": f"eval ({EXE} -m conda shell.fish hook {DEV_ARG})",
-            "print_env_var": "echo $%s",
-        },
-        # We don't know if the PowerShell executable is called
-        # powershell, pwsh, or pwsh-preview.
-        "powershell": {
-            "activator": "powershell",
-            "args": ("-NoProfile", "-NoLogo"),
-            "init_command": (
-                f"{sys.executable} -m conda shell.powershell hook --dev "
-                "| Out-String "
-                "| Invoke-Expression "
-                # want CONDA_SHLVL=0 before running tests so deactivate any active environments
-                # since we do not know how many environments have been activated by the user/CI
-                # just to be safe deactivate a few times
-                "; conda deactivate "
-                "; conda deactivate "
-                "; conda deactivate "
-                "; conda deactivate"
-            ),
-            "print_env_var": "$Env:%s",
-            "exit_cmd": "exit",
-        },
-        "pwsh": {"base_shell": "powershell"},
-        "pwsh-preview": {"base_shell": "powershell"},
-    }
-
-    def __call__(self, shell_name: str):
-        return super().__call__(
-            shell_name,
-            **{
-                **self.SHELLS.get(self.SHELLS[shell_name].get("base_shell"), {}),
-                **self.SHELLS[shell_name],
-            },
-        )
-
-
-class InteractiveShell(metaclass=InteractiveShellType):
-    def __init__(
-        self,
-        shell_name: str,
-        *,
-        activator: str,
-        args: Iterable[str] = (),
-        init_command: str,
-        print_env_var: str,
-        exit_cmd: str | None = None,
-        base_shell: str | None = None,  # ignored
-    ):
-        self.shell_name = shell_name
-        assert (path := which(shell_name))
-        self.shell_exe = quote_for_shell(path, *args)
-        self.shell_dir = dirname(path)
-
-        self.activator = activator_map[activator]()
-        self.args = args
-        self.init_command = init_command
-        self.print_env_var = print_env_var
-        self.exit_cmd = exit_cmd
-
-    def __enter__(self):
-        self.p = PopenSpawn(
-            self.shell_exe,
-            timeout=30,
-            maxread=5000,
-            searchwindowsize=None,
-            logfile=sys.stdout,
-            cwd=os.getcwd(),
-            env={
-                **os.environ,
-                "CONDA_AUTO_ACTIVATE_BASE": "false",
-                "CONDA_AUTO_STACK": "0",
-                "CONDA_CHANGEPS1": "true",
-                # "CONDA_ENV_PROMPT": "({default_env}) ",
-                "PYTHONPATH": self.path_conversion(CONDA_SOURCE_ROOT),
-                "PATH": self.activator.pathsep_join(
-                    self.path_conversion(
-                        (
-                            *self.activator._get_starting_path_list(),
-                            self.shell_dir,
-                        )
-                    )
-                ),
-                # ensure PATH is shared with any msys2 bash shell, rather than starting fresh
-                "MSYS2_PATH_TYPE": "inherit",
-                "CHERE_INVOKING": "1",
-            },
-            encoding="utf-8",
-            codec_errors="strict",
-        )
-
-        if self.init_command:
-            self.p.sendline(self.init_command)
-
-        self.clear()
-
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        if exc_type is not None:
-            print(f"Exception encountered: ({exc_type}) {exc_val}", file=sys.stderr)
-
-        if self.p:
-            if self.exit_cmd:
-                self.sendline(self.exit_cmd)
-
-            self.p.kill(SIGINT)
-
-    def sendline(self, *args, **kwargs):
-        return self.p.sendline(*args, **kwargs)
-
-    def expect(self, *args, **kwargs):
-        try:
-            return self.p.expect(*args, **kwargs)
-        except Exception:
-            print(f"{self.p.before=}", file=sys.stderr)
-            print(f"{self.p.after=}", file=sys.stderr)
-            raise
-
-    def expect_exact(self, *args, **kwargs):
-        try:
-            return self.p.expect_exact(*args, **kwargs)
-        except Exception:
-            print(f"{self.p.before=}", file=sys.stderr)
-            print(f"{self.p.after=}", file=sys.stderr)
-            raise
-
-    def assert_env_var(self, env_var, value, use_exact=False):
-        # value is actually a regex
-        self.sendline(self.print_env_var % env_var)
-        if use_exact:
-            self.expect_exact(value)
-            self.clear()
-        else:
-            self.expect(rf"{value}\r?\n")
-
-    def get_env_var(self, env_var, default=None):
-        self.sendline(self.print_env_var % env_var)
-        if self.shell_name == "cmd.exe":
-            self.expect(rf"@ECHO %{env_var}%\r?\n([^\r\n]*)\r?\n")
-        elif self.shell_name in ("powershell", "pwsh"):
-            self.expect(rf"\$Env:{env_var}\r?\n([^\r\n]*)\r?\n")
-        else:
-            marker = f"get_env_var-{uuid4().hex}"
-            self.sendline(f"echo {marker}")
-            self.expect(rf"([^\r\n]*)\r?\n{marker}\r?\n")
-
-        value = self.p.match.group(1)
-        return default if value is None else value
-
-    def clear(self) -> None:
-        marker = f"clear-{uuid4().hex}"
-        self.sendline(f"echo {marker}")
-        self.expect(rf"{marker}\r?\n")
-
-    def path_conversion(self, *args, **kwargs):
-        return self.activator.path_conversion(*args, **kwargs)
 
 
 def which_powershell():
